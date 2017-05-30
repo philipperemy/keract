e60bee1e6c11395d1d44b224d93faab112247eb6
philipperemy <premy@reactive.co.jp>
2017-05-30 14:22:11 +0900

    enhancement: multi inputs model are now supported!
    Former-commit-id: e11360327be52286d8edf4ac5dede74d63d97875

--- model_train.py
+++ model_train.py
@@ -1,11 +1,6 @@
-'''Trains a simple convnet on the MNIST dataset.
-
-Gets to 99.25% test accuracy after 12 epochs
-(there is still a lot of margin for parameter tuning).
-16 seconds per epoch on a GRID K520 GPU.
-'''
-
 from __future__ import print_function
+
+from glob import glob
 
 import keras
 from keras.callbacks import ModelCheckpoint
@@ -14,51 +9,84 @@ from keras.layers import Dense, Dropout, Flatten
 from keras.models import Sequential
 
 from data import get_mnist_data, num_classes, input_shape
+from read_activations import get_activations
 
 if __name__ == '__main__':
 
-    x_train, y_train, x_test, y_test = get_mnist_data()
+    checkpoints = glob('checkpoints/*.h5')
+    # pip3 install natsort
+    from natsort import natsorted
 
-    model = Sequential()
-    model.add(Conv2D(32, kernel_size=(3, 3),
-                     activation='relu',
-                     input_shape=input_shape))
-    model.add(Conv2D(64, (3, 3), activation='relu'))
-    model.add(MaxPooling2D(pool_size=(2, 2)))
-    model.add(Dropout(0.25))
-    model.add(Flatten())
-    model.add(Dense(128, activation='relu'))
-    model.add(Dropout(0.5))
-    model.add(Dense(num_classes, activation='softmax'))
+    from keras.models import load_model
 
-    model.compile(loss=keras.losses.categorical_crossentropy,
-                  optimizer=keras.optimizers.Adadelta(),
-                  metrics=['accuracy'])
+    if len(checkpoints) > 0:
 
-    # Change starts here
-    import shutil
-    import os
+        checkpoints = natsorted(checkpoints)
+        assert len(checkpoints) != 0, 'No checkpoints found.'
+        checkpoint_file = checkpoints[-1]
+        print('Loading [{}]'.format(checkpoint_file))
+        model = load_model(checkpoint_file)
 
-    # delete folder and its content and creates a new one.
-    try:
-        shutil.rmtree('checkpoints')
-    except:
-        pass
-    os.mkdir('checkpoints')
+        model.compile(optimizer='adam',
+                      loss='categorical_crossentropy',
+                      metrics=['accuracy'])
 
-    checkpoint = ModelCheckpoint(monitor='val_acc',
-                                 filepath='checkpoints/model_{epoch:02d}_{val_acc:.3f}.h5',
-                                 save_best_only=True)
+        print(model.summary())
 
-    model.fit(x_train, y_train,
-              batch_size=128,
-              epochs=12,
-              verbose=1,
-              validation_data=(x_test, y_test),
-              callbacks=[checkpoint])
+        x_train, y_train, x_test, y_test = get_mnist_data()
 
-    # Change finishes here
+        # checking that the accuracy is the same as before 99% at the first epoch.
+        test_loss, test_acc = model.evaluate(x_test, y_test, verbose=1, batch_size=128)
+        print('')
+        assert test_acc > 0.98
 
-    score = model.evaluate(x_test, y_test, verbose=0)
-    print('Test loss:', score[0])
-    print('Test accuracy:', score[1])
+        get_activations(model, x_test[0:1], print_shape_only=True)  # with just one sample.
+
+        get_activations(model, x_test[0:200], print_shape_only=True)  # with 200 samples.
+
+    else:
+        x_train, y_train, x_test, y_test = get_mnist_data()
+
+        model = Sequential()
+        model.add(Conv2D(32, kernel_size=(3, 3),
+                         activation='relu',
+                         input_shape=input_shape))
+        model.add(Conv2D(64, (3, 3), activation='relu'))
+        model.add(MaxPooling2D(pool_size=(2, 2)))
+        model.add(Dropout(0.25))
+        model.add(Flatten())
+        model.add(Dense(128, activation='relu'))
+        model.add(Dropout(0.5))
+        model.add(Dense(num_classes, activation='softmax'))
+
+        model.compile(loss=keras.losses.categorical_crossentropy,
+                      optimizer=keras.optimizers.Adadelta(),
+                      metrics=['accuracy'])
+
+        # Change starts here
+        import shutil
+        import os
+
+        # delete folder and its content and creates a new one.
+        try:
+            shutil.rmtree('checkpoints')
+        except:
+            pass
+        os.mkdir('checkpoints')
+
+        checkpoint = ModelCheckpoint(monitor='val_acc',
+                                     filepath='checkpoints/model_{epoch:02d}_{val_acc:.3f}.h5',
+                                     save_best_only=True)
+
+        model.fit(x_train, y_train,
+                  batch_size=128,
+                  epochs=12,
+                  verbose=1,
+                  validation_data=(x_test, y_test),
+                  callbacks=[checkpoint])
+
+        # Change finishes here
+
+        score = model.evaluate(x_test, y_test, verbose=0)
+        print('Test loss:', score[0])
+        print('Test accuracy:', score[1])
--- read_activations.py
+++ read_activations.py
@@ -1,24 +1,38 @@
 from glob import glob
 
-import keras.backend as K
-import numpy as np
 from keras.models import load_model
 from natsort import natsorted
 
 from data import get_mnist_data
 
 
-def get_activations(model, inputs, print_shape_only=False):
+def get_activations(model, model_inputs, print_shape_only=False, layer_name=None):
+    import keras.backend as K
     print('----- activations -----')
     activations = []
     inp = model.input
-    outputs = [layer.output for layer in model.layers]  # all layer outputs
-    funcs = [K.function([inp] + [K.learning_phase()], [out]) for out in outputs]  # evaluation functions
-    if len(inputs.shape) == 3:
-        batch_inputs = inputs[np.newaxis, ...]
+
+    model_multi_inputs_cond = True
+    if not isinstance(inp, list):
+        # only one input! let's wrap it in a list.
+        inp = [inp]
+        model_multi_inputs_cond = False
+
+    outputs = [layer.output for layer in model.layers if
+               layer.name == layer_name or layer_name is None]  # all layer outputs
+
+    funcs = [K.function(inp + [K.learning_phase()], [out]) for out in outputs]  # evaluation functions
+
+    if model_multi_inputs_cond:
+        list_inputs = []
+        list_inputs.extend(model_inputs)
+        list_inputs.append(1.)
     else:
-        batch_inputs = inputs
-    layer_outputs = [func([batch_inputs, 1.])[0] for func in funcs]
+        list_inputs = [model_inputs, 1.]
+
+    # Learning phase. 1 = Test mode (no dropout or batch normalization)
+    # layer_outputs = [func([model_inputs, 1.])[0] for func in funcs]
+    layer_outputs = [func(list_inputs)[0] for func in funcs]
     for layer_activations in layer_outputs:
         activations.append(layer_activations)
         if print_shape_only:
@@ -26,28 +40,3 @@ def get_activations(model, inputs, print_shape_only=False):
         else:
             print(layer_activations)
     return activations
-
-
-if __name__ == '__main__':
-    checkpoints = natsorted(glob('checkpoints/*.h5'))
-    assert len(checkpoints) != 0, 'No checkpoints found.'
-    checkpoint_file = checkpoints[-1]
-    print('Loading [{}]'.format(checkpoint_file))
-    model = load_model(checkpoint_file)
-
-    model.compile(optimizer='adam',
-                  loss='categorical_crossentropy',
-                  metrics=['accuracy'])
-
-    print(model.summary())
-
-    x_train, y_train, x_test, y_test = get_mnist_data()
-
-    # checking that the accuracy is the same as before 99% at the first epoch.
-    test_loss, test_acc = model.evaluate(x_test, y_test, verbose=1, batch_size=128)
-    print('')
-    assert test_acc > 0.98
-
-    get_activations(model, x_test[0], print_shape_only=True)  # with just one sample.
-
-    get_activations(model, x_test[0:200], print_shape_only=True)  # with 200 samples.
