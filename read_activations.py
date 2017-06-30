65a15d49be075f40f528f1ab1a0721fe482f382b
philipperemy <premy@reactive.co.jp>
2017-06-30 11:11:01 +0900

    commit

--- read_activations.py
+++ read_activations.py
@@ -1,11 +1,3 @@
-from glob import glob
-
-from keras.models import load_model
-from natsort import natsorted
-
-from data import get_mnist_data
-
-
 def get_activations(model, model_inputs, print_shape_only=False, layer_name=None):
     import keras.backend as K
     print('----- activations -----')
@@ -40,3 +32,39 @@ def get_activations(model, model_inputs, print_shape_only=False, layer_name=None
         else:
             print(layer_activations)
     return activations
+
+
+def display_activations(activation_maps):
+    import numpy as np
+    import matplotlib.pyplot as plt
+    """
+    (1, 26, 26, 32)
+    (1, 24, 24, 64)
+    (1, 12, 12, 64)
+    (1, 12, 12, 64)
+    (1, 9216)
+    (1, 128)
+    (1, 128)
+    (1, 10)
+    """
+    batch_size = activation_maps[0].shape[0]
+    assert batch_size == 1, 'One image at a time to visualize.'
+    for i, activation_map in enumerate(activation_maps):
+        print('Displaying activation map {}'.format(i))
+        shape = activation_map.shape
+        if len(shape) == 4:
+            activations = np.hstack(np.transpose(activation_map[0], (2, 0, 1)))
+        elif len(shape) == 2:
+            # try to make it square as much as possible. we can skip some activations.
+            activations = activation_map[0]
+            num_activations = len(activations)
+            if num_activations > 1024:  # too hard to display it on the screen.
+                square_param = int(np.floor(np.sqrt(num_activations)))
+                activations = activations[0: square_param * square_param]
+                activations = np.reshape(activations, (square_param, square_param))
+            else:
+                activations = np.expand_dims(activations, axis=0)
+        else:
+            raise Exception('len(shape) = 3 has not been implemented.')
+        plt.imshow(activations, interpolation='None', cmap='jet')
+        plt.show()
