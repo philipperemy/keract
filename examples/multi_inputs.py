190a3cc6ccf50e8e2ec089c4efc27087e6d7df07
philipperemy <premy@cogent.co.jp>
2018-10-05 20:34:47 +0900

    finalization

--- examples/multi_inputs.py
+++ examples/multi_inputs.py
@@ -23,8 +23,6 @@ def get_single_inputs_model():
 
 
 if __name__ == '__main__':
-    m = get_multi_inputs_model()
-
     np.random.seed(123)
     inp_a = np.random.uniform(size=(5, 10))
     inp_b = np.random.uniform(size=(5, 10))
@@ -32,17 +30,15 @@ if __name__ == '__main__':
     # Just for visual purposes.
     np.set_printoptions(precision=2)
 
-    # MULTI-INPUT MODEL
-
     # Activations of all the layers
     print('MULTI-INPUT MODEL')
-    utils.print_names_and_values(get_activations(m, [inp_a, inp_b]))
+    m1 = get_multi_inputs_model()
+    utils.print_names_and_values(get_activations(m1, [inp_a, inp_b]))
 
     # Just get the last layer!
-    print(get_activations(m, [inp_a, inp_b], layer_name='last_layer'))
+    print(get_activations(m1, [inp_a, inp_b], layer_name='last_layer'))
     print('')
 
-    # SINGLE-INPUT MODEL
-    m2 = get_single_inputs_model()
     print('SINGLE-INPUT MODEL')
+    m2 = get_single_inputs_model()
     utils.print_names_and_values(get_activations(m2, inp_a))
