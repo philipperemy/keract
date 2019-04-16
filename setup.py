6a67784034412c5203637357fa8cc4c6909f93ab
philipperemy <premy@cogent.co.jp>
2019-04-16 09:53:08 +0900

    2.5.2

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='keract',
-    version='2.5.1',
+    version='2.5.2',
     description='Keras Activations and Gradients',
     author='Philippe Remy',
     license='MIT',
