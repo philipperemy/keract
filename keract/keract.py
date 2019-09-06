30fcb47b155497b413da834dd406c1e398fae2d4
zkx741481546 <zkx741481546@live.com>
2019-09-06 20:05:30 +0800

    Add function: 1. different data_format of channel 2. picture size changed to 24x24
    Fixed:
    when len(shape)=2 draw picture wrong, now when len(shape)=2, keract won't draw channel
    
    changed:
    keract/keract.py ---display_activations()
    added param data_format
    added dir for path to save picture, example:'pic/step1/'

--- keract/keract.py
+++ keract/keract.py
@@ -85,7 +85,7 @@ def get_activations(model, x, layer_name=None):
     return result
 
 
-def display_activations(activations, cmap=None, save=False, dir=''):
+def display_activations(activations, cmap=None, save=False, dir='',data_format='channels_last'):
     """
     Plot the activations for each layer using matplotlib
     :param activations: dict mapping layers to corresponding activations (1, output_h, output_w, num_filters)
@@ -100,28 +100,44 @@ def display_activations(activations, cmap=None, save=False, dir=''):
         if acts.shape[0] != 1:
             print('-> Skipped. First dimension is not 1.')
             continue
-        """
+
+        print('')
+        # channel first
+        if data_format == 'channels_last':
+            c=-1
+        elif data_format == 'channels_first':
+            c=1
+        nrows = int(math.sqrt(acts.shape[c]) - 0.001) + 1  # best square fit for the given number
+        ncols = int(math.ceil(acts.shape[c] / nrows))
+        fig, axes = plt.subplots(nrows, ncols, squeeze=False, figsize=(24, 24))
+        fig.suptitle(layer_name)
         if len(acts.shape) <= 2:
+            """
             print('-> Skipped. 2D Activations.')
             continue
-        """
-        print('')
-        nrows = int(math.sqrt(acts.shape[-1]) - 0.001) + 1  # best square fit for the given number
-        ncols = int(math.ceil(acts.shape[-1] / nrows))
-        fig, axes = plt.subplots(nrows, ncols, squeeze=False, figsize=(12, 12))
-        fig.suptitle(layer_name)
-        for i in range(nrows * ncols):
-            if i < acts.shape[-1]:
-                if len(acts.shape)==2:
-                    img = acts[0, i]
-                    hmap = axes.flat[i].imshow([[img]], cmap=cmap)
-                elif len(acts.shape)==3:
-                    img = acts[0, :, i]
-                    hmap = axes.flat[i].imshow([img], cmap=cmap)
-                elif len(acts.shape)==4:
-                    img = acts[0, :, :, i]
-                    hmap = axes.flat[i].imshow(img, cmap=cmap)
-            axes.flat[i].axis('off')
+            """
+            # no channel
+            fig, axes = plt.subplots(1, 1, squeeze=False, figsize=(24, 24))
+            fig.suptitle(layer_name)
+            img = acts[0, :]
+            hmap = axes.flat[0].imshow([img], cmap=cmap)
+            axes.flat[0].axis('off')
+        else:
+            for i in range(nrows * ncols):
+                if i < acts.shape[-1]:
+                    if len(acts.shape)==3:
+                        if data_format == 'channels_last':
+                            img = acts[0, :, i]
+                        elif data_format == 'channels_first':
+                            img = acts[0, i, :]
+                        hmap = axes.flat[i].imshow([img], cmap=cmap)
+                    elif len(acts.shape)==4:
+                        if data_format=='channels_last':
+                            img = acts[0, :, :, i]
+                        elif data_format=='channels_first':
+                            img = acts[0, i, :, :]
+                        hmap = axes.flat[i].imshow(img, cmap=cmap)
+                axes.flat[i].axis('off')
         fig.subplots_adjust(right=0.8)
         cbar = fig.add_axes([0.85, 0.15, 0.03, 0.7])
         fig.colorbar(hmap, cax=cbar)
