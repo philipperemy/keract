15431e57a555f473a8f6314a7e4236caf605380f
Philippe Remy <premy.enseirb@gmail.com>
2019-12-15 15:28:25 +0900

    update flake8

--- keract/__init__.py
+++ keract/__init__.py
@@ -1,10 +1,10 @@
-from keract.keract import display_activations
-from keract.keract import display_gradients_of_trainable_weights
-from keract.keract import display_heatmaps
-from keract.keract import get_activations
-from keract.keract import get_gradients_of_activations
-from keract.keract import get_gradients_of_trainable_weights
-from keract.keract import load_activations_from_json_file
-from keract.keract import persist_to_json_file
+from keract.keract import display_activations  # noqa
+from keract.keract import display_gradients_of_trainable_weights  # noqa
+from keract.keract import display_heatmaps  # noqa
+from keract.keract import get_activations  # noqa
+from keract.keract import get_gradients_of_activations  # noqa
+from keract.keract import get_gradients_of_trainable_weights  # noqa
+from keract.keract import load_activations_from_json_file  # noqa
+from keract.keract import persist_to_json_file  # noqa
 
 __version__ = '2.8.5'
