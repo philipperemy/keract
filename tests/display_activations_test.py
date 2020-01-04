59adec8f9f40e6af4364fd0fadc3aef71b0ee35f
Philippe Remy <premy.enseirb@gmail.com>
2020-01-04 12:17:33 +0900

    add one more test

---
+++ tests/display_activations_test.py
@@ -0,0 +1,34 @@
+import os
+import unittest
+from glob import glob
+
+import keras.backend as K
+import numpy as np
+from keras import Input, Model
+from keras.layers import Dense
+
+from keract import get_activations, display_activations
+
+
+def dummy_model_and_inputs():
+    i1 = Input(shape=(10,), name='i1')
+    a = Dense(1, name='fc1')(i1)
+    model = Model(inputs=[i1], outputs=[a])
+    x = np.random.uniform(size=(1, 10))
+    return model, x
+
+
+class DisplayActivationsTest(unittest.TestCase):
+
+    def setUp(self) -> None:
+        K.clear_session()
+
+    def tearDown(self) -> None:
+        K.clear_session()
+        for image in glob('*.png'):
+            os.remove(image)
+
+    def test_display_1(self):
+        model, x = dummy_model_and_inputs()
+        acts = get_activations(model, x)
+        display_activations(acts, save=True)
