bdcc874e82ea3573b08f4f335aa41387dca46482
Philippe Remy <premy.enseirb@gmail.com>
2020-09-09 11:28:37 +0900

    4.3.2

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='keract',
-    version='4.3.1',
+    version='4.3.2',
     description='Keract - Tensorflow Keras Activations and Gradients',
     author='Philippe Remy',
     license='MIT',
